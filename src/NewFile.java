db001a9120b2139c91c8345786b068559990edde
Marcelino Pinheiro <marcelinopinheirods@outlook.com>
2023-12-23 10:53:08 -0300

    New file

--- src/NewFile.java
+++ src/NewFile.java
@@ -1,5 +1,5 @@
 public class NewFile {
     public static void main(String[] args) {
-        System.out.println("Renamed the project and create a new file.");
+        System.out.println("Renamed and create a new file.");
     }
 }
