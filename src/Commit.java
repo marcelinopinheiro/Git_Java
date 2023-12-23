5a970bb488726b8d4fb074c7b33de0101d43045f
Marcelino Pinheiro <marcelinopinheirods@outlook.com>
2023-12-23 09:54:43 -0300

    Renamed the project

--- src/Commit.java
+++
@@ -1,5 +0,0 @@
-public class Commit {
-    public static void main(String[] args) {
-        System.out.println("My first commit!");
-    }
-}
---
+++ src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("My first commit!");
+
+        System.out.println("My second commit in Java!");
+        System.out.println("Renamed the project.");
+    }
+}
